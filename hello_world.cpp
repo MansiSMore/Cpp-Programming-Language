a3da750148ff678a481b970aafa7a880398cd16d
Mansi More <45144625+MansiSMore@users.noreply.github.com>
2021-01-08 12:34:30 +0530

    Add files via upload

---
+++ hello_world.cpp
@@ -0,0 +1,13 @@
+//My First CPP Demo Code.
+//This statement tells a compiler to add or include declarations of the standard stream I/O facilities as found in iostream .
+#include <iostream>
+
+//The std specifies that the name cout is to be found in the standard-library namespace.
+using namespace std;
+
+//Entry point function.
+int main()
+{
+	cout << "Hello World..!" << endl;			//cout is same as printf function from c.
+	return 0;
+}
